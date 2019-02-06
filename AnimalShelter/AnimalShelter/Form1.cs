c95716f021cad3031e1b595c523857f73fb90297
BongUk <hero_come@naver.com>
2019-02-06 17:41:18 +0900

    190206 study

--- AnimalShelter/AnimalShelter/Form1.cs
+++ AnimalShelter/AnimalShelter/Form1.cs
@@ -16,5 +16,18 @@ namespace AnimalShelter
         {
             InitializeComponent();
         }
+
+        private void CreateCustomer_Click(object sender, EventArgs e)
+        {
+            Customer cus = new Customer(CusNewFirstName.Text, CusNewLastName.Text, DateTime.Parse(CusNewBirthday.Text));
+            cus.Address = CusNewAddress.Text;
+            cus.Description = CusNewDescription.Text;
+
+            CusFullName.Text = cus.FullName;
+            CusAge.Text = cus.Age.ToString();
+            CusAddress.Text = cus.Address;
+            CusDec.Text = cus.Description;
+            CusIsQ.Text = cus.IsQualified.ToString();
+        }
     }
 }
